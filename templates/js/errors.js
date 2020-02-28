d9bcb4ad814445144ac8f513e22f91437162e128
Kristian <kn@unisport.dk>
2020-02-28 13:55:16 +0100

    Clean up

--- templates/js/errors.js
+++
@@ -1,7 +0,0 @@
-/**/
-const ErrorMessages = {
-  "ReferenceError: response is not defined": "Ok, so something went wrong...",
-  "generic error message": "Oh shoot, no clue what just happened!"
-};
-
-export { ErrorMessages };
