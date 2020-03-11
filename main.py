48497fcfe2e9e6ff515e0c0155ac193239008c01
Kristian <kn@unisport.dk>
2020-03-11 12:44:45 +0100

    Adding some JS

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import os
 from functools import wraps
 
-from flask import Flask, render_template, request, redirect, jsonify, g
-from google.cloud import datastore
+from flask import Flask, render_template, request, redirect, jsonify
 
-from models import User
-
-env = os.getenv('APP_ENV', 'production')
-
-if env is not 'production':
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/kn/Documents/private/refactored-engine/refactored-engine-9a76290b675f.json'
-
-datastore_client = datastore.Client()
+from models import user_authenticate, user_create, user_lists, list_create
 
 app = Flask(__name__)
 
@@ -23,7 +15,24 @@ def hello():
 @app.route('/signin/', methods=["GET", "POST"])
 def signin():
     if request.method == 'POST':
-        return jsonify(loc='/app/')
+        # TODO: Pull mail and token from request and pass it
+        # to backend
+        if not 'Authorization' in request.headers:
+            return jsonify(message='Not authorized'), 401
+
+        data = request.get_json()
+        token = request.headers['Authorization']
+
+        if not 'email' in data:
+            return jsonify(message='No email provided'), 401
+
+        user = user_authenticate(mail=data['email'])
+
+        if user == None:
+            # If the user/email doesn't exist, create it
+            user = user_create(mail=data['email'])
+
+        return jsonify(loc='/app/', user_id=user.id)
 
     # TODO: create page
     return render_template('signin.html')
@@ -31,6 +40,17 @@ def signin():
 @app.route('/app/')
 def app_index():
     return render_template('app.html')
+
+@app.route('/service/<user_id>/lists/', methods=["GET", "POST"])
+def service_list(user_id=None):
+    if request.method == 'POST':
+        data = request.get_json()
+        list_create(data['list_name'], user_id)
+
+    lists = user_lists(user_id=user_id)
+    print(lists)
+    return jsonify(user_lists=lists)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,62 @@
+import os
+from datetime import datetime
 
-class User():
-    def upsert(self, **kwargs):
-        pass
+from google.cloud import datastore
 
-    def is_user(self):
-        return True
+env = os.getenv('APP_ENV', 'production')
 
-    def authorize(self, **kwargs):
-        pass
+# For development purposes
+if env is not 'production':
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/kn/Documents/private/refactored-engine/refactored-engine-9a76290b675f.json'
+
+datastore_client = datastore.Client()
+# FIXME: rename to email
+def user_create(mail=None):
+    #
+    user_key = datastore_client.key('User')
+    user = datastore.Entity(key=user_key)
+    user['created_at'] = datetime.now()
+    user['last_visit'] = datetime.now()
+    # FIXME: rename to email
+    user['email'] = mail
+
+    datastore_client.put(user)
+
+    return user.key
+
+# TODO: add datetime for last visit
+def user_update(email=None):
+    pass
+
+# FIXME: Should return user Entity or None
+# TODO: if the user exists, update last_visit
+def user_authenticate(mail=None):
+    query = datastore_client.query(kind='User')
+    result = list(query.add_filter('email', '=', mail).fetch(1))[0]
+
+    return result
+
+# Return all lists belonging to the user
+def user_lists(user_id=None):
+    query = datastore_client.query(kind='UserList')
+    result = list(query.add_filter('user_id', '=', user_id).fetch())
+
+    return result
+
+# Create a new list
+def list_create(name=None, user_id=None):
+    list_key = datastore_client.key('UserList')
+    user_list = datastore.Entity(key=list_key)
+    user_list['name'] = name
+    user_list['user_id'] = user_id
+    user_list['created_at'] = datetime.now()
+    user_list['items'] = []
+
+    datastore_client.put(user_list)
+
+    return user_list.key
+
+# Model to share lists between users
+def user_list_user(owner_id=None, list_id=None, user_id=None):
+    pass
+
