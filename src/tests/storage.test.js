53b2cb22b55e85879085889760e5960953f217ad
Kristian <kn@unisport.dk>
2020-03-20 11:48:37 +0100

    Adding permission localstorage feature

--- src/tests/storage.test.js
+++ src/tests/storage.test.js
@@ -1,7 +1,27 @@
 /**
  * storage.test.js
  */
-import {sum} from "./../lib/storage.js"
+jest.spyOn(window.localStorage.__proto__, 'setItem');
+jest.spyOn(window.localStorage.__proto__, 'getItem');
 
-test("adds 1 + 2 to equal 3", () =>
-  expect(sum(1, 2)).toBe(3))
+import { storeItem, fetchItem } from "./../lib/storage.js"
+
+test("returns true when item is stored", () => {
+  expect.assertions(1);
+  return storeItem('test', {hello: 'Kitty'}).then(len =>
+    expect(len).toBe(17))
+})
+
+test("return error when item cannot be fetched", () => {
+  expect.assertions(1)
+  return fetchItem('hello').catch(e =>
+    expect(e).toBe('hello not found'))
+})
+
+test("return object when item can be fetched", () => {
+  let dummy = {hello: 'Kitty'}
+
+  expect.assertions(1)
+  return fetchItem('test').then(obj => 
+    expect(obj.hello).toBe(dummy.hello))
+})
