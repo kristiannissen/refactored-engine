0f999aa5698f5c403cbe9af625832c7bb4447dfc
Kristian <kn@unisport.dk>
2020-03-30 12:56:30 +0200

    Working on #4

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -28,7 +28,18 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-  console.log("sw fetch", event);
+  if (event.request.method == 'GET') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request).then(response => {
+            cache.put(event.request, response.clone())
+            return response
+          })
+        })
+      })
+    )
+  }
 });
 
 self.addEventListener('sync', event =>
