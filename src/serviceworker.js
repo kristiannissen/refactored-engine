b5693177af68e5e26bf787590a5339bc5c67eb17
Kristian <kn@unisport.dk>
2020-04-01 15:30:17 +0200

    Working on #16

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -18,6 +18,8 @@ let assets = [
 self.addEventListener("install", event => {
   console.log("sw install", event);
 
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
@@ -28,23 +30,47 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   console.log("sw activate", event);
 
+  self.clients.claim();
+
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName !== CACHE_NAME
-        }).map(cacheName => {
-          return caches.delete(cacheName)
-        })
-      )
+        cacheNames
+          .filter(cacheName => {
+            return cacheName !== CACHE_NAME;
+          })
+          .map(cacheName => {
+            return caches.delete(cacheName);
+          })
+      );
     })
-  )
+  );
 });
 
 self.addEventListener("fetch", event => {
-  // console.log("sw fetch", event);
+  console.log("sw fetch", event.request.url);
 
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(resp => {
+        return (
+          resp ||
+          fetch(event.request).then(response => {
+            if (event.request.method === "GET") {
+              let respClone = response.clone();
+
+              caches
+                .open(CACHE_NAME)
+                .then(cache => cache.put(event.request, respClone));
+            }
+
+            return response;
+          })
+        );
+      })
+      .catch(error => console.log("post", error))
+  );
 });
 
 self.addEventListener("sync", event => console.log("sw sync", event));
